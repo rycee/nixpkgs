b92c0e46f91f5a67075fae90505746a27ce25c46
Robert Helgesson <robert@rycee.net>
2020-02-01 19:47:21 +0100

    k9s: 0.9.3 -> 0.13.6
    (cherry picked from commit e366e3a0337daa340baf1b98f7a55c3e88cb7541)

--- pkgs/applications/networking/cluster/k9s/default.nix
+++ pkgs/applications/networking/cluster/k9s/default.nix
@@ -2,17 +2,18 @@
 
 buildGoModule rec {
   pname = "k9s";
-  version = "0.7.13";
+  version = "0.13.6";
+  # rev is the release commit, mainly for version command output
+  rev = "8fedc42304ce33df314664eb0c4ac73be59065af";
 
   src = fetchFromGitHub {
-    owner = "derailed";
-    repo = "k9s";
-    rev = version;
-    sha256 = "0wsj6wc2qi5708cg47l2qblq1cg8fcwxdygpkayib9hapx6lc6f8";
+    owner  = "derailed";
+    repo   = "k9s";
+    rev    = "v${version}";
+    sha256 = "1gffbj6pgys6k3i8ikcy3yr2r9cwg0xki55yz5yg6z4a8h0jc8a6";
   };
 
-  modSha256 = "1ia9wx6yd9mdr981lcw58xv39iqzz25r03bmn1c6byxmq2xpcjq8";
-
+  modSha256 = "04k1wfhyignxy84pvq09fxvvk7pxdswbrzxvxc50dz8n8y7wcnjf";
 
   meta = with stdenv.lib; {
     description = "Kubernetes CLI To Manage Your Clusters In Style.";
