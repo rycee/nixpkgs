9bbcbf0693340253a0ff584f53fc91f16d394d8b
InfinityGhost <infinity@opentabletdriver.net>
2025-03-04 13:01:11 -0500

    pkgs-lib.formats.xml: Fix withHeader parameter
    (cherry picked from commit 07bcf3e909f5db0dcef5bd31885aab3fdeae1d7b)

--- pkgs/pkgs-lib/formats.nix
+++ pkgs/pkgs-lib/formats.nix
@@ -767,7 +767,7 @@ rec {
                   import xmltodict
 
                   with open(os.environ["valuePath"], "r") as f:
-                      print(xmltodict.unparse(json.load(f), full_document=${toString withHeader}, pretty=True, indent=" " * 2))
+                      print(xmltodict.unparse(json.load(f), full_document=${if withHeader then "True" else "False"}, pretty=True, indent=" " * 2))
                 '';
                 passAsFile = [
                   "value"
